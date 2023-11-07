36cdd9eb24c0691b0215cd0ac349ce9af0cb133e
johanmoback <mobackjohan@gmail.com>
2023-11-07 20:50:36 +0100

    prettify json response

--- load-config.py
+++ load-config.py
@@ -1,9 +1,14 @@
+import json
+
 import yaml
 import requests
+from rocketapi import send_requests
 
 # Load the YAML file with test cases
 with open("config.yaml", "r") as config_file:
     config = yaml.safe_load(config_file)
+
+responses = {}
 
 # Loop through test cases and execute requests
 for test_case_name, test_case_params in config.items():
@@ -17,11 +22,17 @@ for test_case_name, test_case_params in config.items():
     elif method == "GET":
         response = requests.get(endpoint)
 
+    responses[test_case_name] = {
+        "status_code": response.status_code,
+        "content": response.text
+    }
+
     # Process the response as needed
     if response.status_code == 200:
         print(f"Test Case: {test_case_name} - Successful")
-        # Add additional processing for successful responses
+        print((json.dumps(responses, indent=4)))
+
     else:
         print(f"Test Case: {test_case_name} - Failed (Status Code: {response.status_code})")
         print(response.text)  # Display response content for error details
-        # Add error handling and logging for failed responses
+
